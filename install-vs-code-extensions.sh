89608de44c2fa7420db972a72ac70467612b7876
AdityaDutta02 <85363830+AdityaDutta02@users.noreply.github.com>
2022-03-14 20:59:01 +0530

    Update install-vs-code-extensions.sh

--- install-vs-code-extensions.sh
+++ install-vs-code-extensions.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl https://alfredmyers.github.io/codex/all.txt | xargs -L 1 code --install-extension
+curl https://AdityaDutta02.github.io/codex/all.txt | xargs -L 1 code --install-extension
